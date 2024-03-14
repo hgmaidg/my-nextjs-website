feb64ab2eb313fa3b4dea70504772ec1980f34de
Hoang Mai <hgmaidg@gmail.com>
2024-03-14 09:33:33 +0700

    create folders structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+const name = "HM"
+const string = `Her name is ${name}`;
 const Home = () => {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Home</h1>
+      <p>{string}</p>
     </main>
   );
 };
-
 export default Home;
