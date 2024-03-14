8ca8830c28b230e75e5306838a9edfb055523c94
Hoang Mai <hgmaidg@gmail.com>
2024-03-14 11:41:23 +0700

    base NavBar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import NavLink from "./navLinks/navLinks";
+import styles from "./links.module.css";
 const Links = () => {
   const links = [
     {
@@ -18,13 +20,22 @@ const Links = () => {
       path: "/blog",
     },
   ];
+  const session = true;
+  const isAdmin = true;
+
   return (
-    <div>
+    <div className={styles.links}>
       {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
+        <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/login" }} />
+      )}
     </div>
   );
 };
