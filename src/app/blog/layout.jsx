844d7479a6b2a855dd2a97892d6de9b0a86a54e3
Mai Duong <113920406+hgmaidg@users.noreply.github.com>
2024-03-22 11:32:02 +0700

    style blogs page

--- src/app/blog/layout.jsx
+++
@@ -1,7 +0,0 @@
-const BlogLayout = ({children}) => {
-    return (
-        <h2>This is the blog layout</h2>
-    );
-}
-
-export default BlogLayout;
---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "./postCard.module.css";
+import Image from "next/image";
+const PostCard = () => {
+    return <div className={styles.container}>
+        <div className={styles.top}>
+            <div className={styles.imgContainer}>
+                <Image className={styles.img} src="https://images.pexels.com/photos/8755970/pexels-photo-8755970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill></Image>
+            </div>
+            <span className={styles.date}>03.22.2024</span>
+        </div>
+        <div className={styles.bottom}>
+            <h1 className={styles.title}>Title</h1>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, quos.</p>
+            <Link href="/blog/1" className={styles.link}>Read More</Link>
+        </div>
+
+    </div>;
+};
+export default PostCard
